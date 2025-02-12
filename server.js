b815db0586dbc8d12be38c375609e2e898df1a9f
Victor Soares <victor9soares@gmail.com>
2025-02-12 11:43:47 -0300

    feat: Docker up

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/users", userRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 sequelize.sync().then(() => {
   app.listen(PORT, () => {
