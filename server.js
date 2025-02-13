3d047922f99daf979b64231e401c59b1d0873c35
Victor Soares <victor9soares@gmail.com>
2025-02-13 12:42:39 -0300

    refact: ajuste link css

--- server.js
+++ server.js
@@ -18,11 +18,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
 
-sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-  });
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
@@ -50,3 +46,9 @@ try {
         res.status(500).json({ mensagem: "Erro interno no servidor." });
     }
 });
+
+sequelize.sync().then(() => {
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
+    });
+  });
