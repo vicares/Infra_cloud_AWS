35c581becf91501b26d19e2985850f8330bebd11
Victor Soares <victor9soares@gmail.com>
2025-02-13 09:22:07 -0300

    feat: link html

--- models/user.js
+++ models/user.js
@@ -1,25 +1,19 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  User.init({
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    role: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
+  const User = sequelize.define('User', {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    senha: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    tipo: {
+      type: DataTypes.ENUM('aluno', 'professor'),
+      allowNull: false,
+    },
   });
+
   return User;
 };
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const path = require('path');
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const { sequelize } = require("./models");
 const userRoutes = require("./routes/userRoutes");
+
+const { User } = require('./models');
 
 require("dotenv").config();
 
@@ -10,6 +13,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/users", userRoutes);
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
 
@@ -17,4 +22,31 @@ sequelize.sync().then(() => {
   app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
   });
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.post('/api/login', async (req, res) => {
+    const { email, senha, tipo } = req.body;
+
+try {
+        // Verifique se o usuário existe e se a senha está correta
+        const usuario = await User.findOne({
+            where: {
+                email: email,
+                tipo: tipo
+            }
+        });
+
+        if (usuario && usuario.senha === senha) {
+            res.json({ mensagem: "Login bem-sucedido!" });
+        } else {
+            res.status(401).json({ mensagem: "Credenciais inválidas." });
+        }
+    } catch (error) {
+        console.error('Erro no login:', error);
+        res.status(500).json({ mensagem: "Erro interno no servidor." });
+    }
 });
